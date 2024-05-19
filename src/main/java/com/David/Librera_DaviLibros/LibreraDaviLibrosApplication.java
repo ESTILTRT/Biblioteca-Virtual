bf54cfc3c31fe99d67cfc65bc3b5b127235122bc
David Campos <david060304@gmail.com>
2024-05-18 22:28:53 -0600

    Generación de los archivos importantes y colocacion de las propiedades del proyecto

---
+++ src/main/java/com/David/Librera_DaviLibros/LibreraDaviLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.David.Librera_DaviLibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreraDaviLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreraDaviLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/David/Librera_DaviLibros/LibreraDaviLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.David.Librera_DaviLibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreraDaviLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
